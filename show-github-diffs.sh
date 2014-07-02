dbb1d40c014ee0ba677d23db0f6af85976a51468
Peter Urda <peter.urda@gmail.com>
2014-07-02 12:40:03 -0700

    Rename tool, add tool to check github diff

---
+++ show-github-diffs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+diff-bash() {
+    # If colordiff is available, use it in place of diff
+    if hash colordiff 2>/dev/null; then
+        colordiff -u $@
+    else
+        diff -u $@
+    fi
+}
+
+diff-bash --label=GitHub <(curl -s https://raw.githubusercontent.com/urda/urda.bash/master/bashrc) ~/.bashrc
+#diff-bash --label=GitHub <(curl -s https://raw.githubusercontent.com/urda/urda.bash/master/bash_aliases) ~/.bash_aliases
+diff-bash --label=GitHub <(curl -s https://raw.githubusercontent.com/urda/urda.bash/master/bash_exports) ~/.bash_exports
+diff-bash --label=GitHub <(curl -s https://raw.githubusercontent.com/urda/urda.bash/master/bash_profile) ~/.bash_profile
